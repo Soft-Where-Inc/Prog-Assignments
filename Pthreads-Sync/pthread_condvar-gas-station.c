94627d0f7032edce9a5190e37ccf32d783c5a5c8
Aditya Gurajada <agurajada@vmware.com>
2023-04-03 08:59:16 -0700

    Working version w/cond-var-signal enabled.

--- Pthreads-Sync/pthread_condvar-gas-station.c
+++ Pthreads-Sync/pthread_condvar-gas-station.c
@@ -5,10 +5,10 @@
  *   https://code-vault.net/lesson/18ec1942c2da46840693efe9b51ff44a
  *  Video: https://www.youtube.com/watch?v=0sVGnxg6Z3k
  */
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <errno.h>
 
 /* Configuration */
@@ -19,14 +19,14 @@
 #define MIN_FUEL_AVAILABLE       40
 
 // # of threads filling gas tank
-#define NUM_FILLERS              2
+#define NUM_THREADS              2
 
 // # of cars drawing fuel at on time. (Think lanes)
-#define NUM_CARS                 NUM_FILLERS
+#define NUM_CARS                 NUM_THREADS
 
-pthread_mutex_t mutexFuel;
-pthread_cond_t condFuel;
 int fuel = 0;
+pthread_mutex_t mutexFuel; // To manage 'fuel' capacity
+pthread_cond_t condFuel;   // To synchronize between filler & user
 
 /*
  * Producer: Fill fuel into fuel tank at gas station.
@@ -37,31 +37,55 @@ int fuel = 0;
 void *
 fuel_filling(void * arg)
 {
+    int thread_id = *(int *) arg;
     for (int i = 0; i < NUM_FILL_LOOPS; i++) {
         pthread_mutex_lock(&mutexFuel);
         fuel += AMOUNT_FILLED_PER_LOOP;
-        printf("Filled fuel... %d\n", fuel);
-        pthread_mutex_unlock(&mutexFuel);
+        printf("[ThreadID=%d] Filled fuel ... Available fuel=%d\n",
+                thread_id, fuel);
+
+        // The order of these two calls seems to not matter for overall
+        // correctness of the program. As long as we unlock the mutex,
+        // the thread receiving the signal can react to the signal.
+        // If we don't unlock, the other thread will just block waiting
+        // to acquire the lock.
         pthread_cond_signal(&condFuel);
+        pthread_mutex_unlock(&mutexFuel);
         sleep(1);
     }
     return NULL;
 }
 
+/*
+ * Consumer: Draw fuel from tank if sufficient fuel exists.
+ *  Wait till there is enough fuel to draw from.
+ */
 void *
 car(void * arg)
 {
+    int thread_id = *(int *) arg;
+
     pthread_mutex_lock(&mutexFuel);
     while (fuel < MIN_FUEL_AVAILABLE) {
-        printf("No fuel. Waiting...\n");
+        printf("[ThreadID=%d] Available fuel=%d is inadequate. Waiting...\n",
+                thread_id, fuel);
+
+        // This seems to work ... but is not good enough.
+        /*
+        pthread_mutex_unlock(&mutexFuel);
+        sleep(1);
+        pthread_mutex_lock(&mutexFuel);
+        */
+        /* This single call is equivalent to:
+         * pthread_mutex_unlock(&mutexFuel);
+         * wait for signal on condFuel
+         * pthread_mutex_lock(&mutexFuel);
+         */
         pthread_cond_wait(&condFuel, &mutexFuel);
-        // Equivalent to:
-        // pthread_mutex_unlock(&mutexFuel);
-        // wait for signal on condFuel
-        // pthread_mutex_lock(&mutexFuel);
     }
+    printf("[ThreadID=%d] Available fuel=%d. Get fuel=%d, fuel left=%d\n",
+            thread_id, fuel, MIN_FUEL_AVAILABLE, (fuel - MIN_FUEL_AVAILABLE));
     fuel -= MIN_FUEL_AVAILABLE;
-    printf("Got fuel. Now left: %d\n", fuel);
     pthread_mutex_unlock(&mutexFuel);
     return NULL;
 }
@@ -69,22 +93,23 @@ car(void * arg)
 int
 main(int argc, char * argv[])
 {
-    pthread_t th[NUM_FILLERS];
+    pthread_t th[NUM_THREADS];
     pthread_mutex_init(&mutexFuel, NULL);
     pthread_cond_init(&condFuel, NULL);
-    for (int i = 0; i < NUM_FILLERS; i++) {
+
+    for (int i = 0; i < NUM_THREADS; i++) {
         if (i == 1) {
-            if (pthread_create(&th[i], NULL, &fuel_filling, NULL) != 0) {
-                perror("Failed to create thread");
+            if (pthread_create(&th[i], NULL, &fuel_filling, (void *) &i) != 0) {
+                perror("Failed to create fuel-filling thread");
             }
         } else {
-            if (pthread_create(&th[i], NULL, &car, NULL) != 0) {
-                perror("Failed to create thread");
+            if (pthread_create(&th[i], NULL, &car, (void *) &i) != 0) {
+                perror("Failed to create thread for car pulling fuel");
             }
         }
     }
 
-    for (int i = 0; i < NUM_FILLERS; i++) {
+    for (int i = 0; i < NUM_THREADS; i++) {
         if (pthread_join(th[i], NULL) != 0) {
             perror("Failed to join thread");
         }
