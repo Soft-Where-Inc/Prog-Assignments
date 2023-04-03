9054eac6e4092f40c35d89637f576304d673f1f4
Aditya Gurajada <agurajada@vmware.com>
2023-04-03 09:39:02 -0700

    Add NUM_CARS, NUM_FILLERS, NUM_THREADS. Report thread-IDs 'tids' in msgs.

--- Pthreads-Sync/pthread_condvar-gas-station.c
+++ Pthreads-Sync/pthread_condvar-gas-station.c
@@ -1,8 +1,7 @@
 /*
  * Conditional Variables & Mutexes: Gas Station example.
  *
- * Ref:
- *   https://code-vault.net/lesson/18ec1942c2da46840693efe9b51ff44a
+ * Ref:    https://code-vault.net/lesson/18ec1942c2da46840693efe9b51ff44a
  *  Video: https://www.youtube.com/watch?v=0sVGnxg6Z3k
  */
 #include <stdio.h>
@@ -19,10 +18,13 @@
 #define MIN_FUEL_AVAILABLE       40
 
 // # of threads filling gas tank
-#define NUM_THREADS              2
+#define NUM_FILLERS              1
 
 // # of cars drawing fuel at on time. (Think lanes)
-#define NUM_CARS                 NUM_THREADS
+#define NUM_CARS                 4
+
+// Total # of threads in the system
+#define NUM_THREADS     (NUM_FILLERS + NUM_CARS)
 
 int fuel = 0;
 pthread_mutex_t mutexFuel; // To manage 'fuel' capacity
@@ -38,20 +40,22 @@ void *
 fuel_filling(void * arg)
 {
     int thread_id = *(int *) arg;
-    for (int i = 0; i < NUM_FILL_LOOPS; i++) {
-        pthread_mutex_lock(&mutexFuel);
-        fuel += AMOUNT_FILLED_PER_LOOP;
-        printf("[ThreadID=%d] Filled fuel ... Available fuel=%d\n",
-                thread_id, fuel);
+    while (1) {
+        for (int i = 0; i < NUM_FILL_LOOPS; i++) {
+            pthread_mutex_lock(&mutexFuel);
+            fuel += AMOUNT_FILLED_PER_LOOP;
+            printf("[ThreadID=%d] Filled fuel ... Available fuel=%d\n",
+                    thread_id, fuel);
 
-        // The order of these two calls seems to not matter for overall
-        // correctness of the program. As long as we unlock the mutex,
-        // the thread receiving the signal can react to the signal.
-        // If we don't unlock, the other thread will just block waiting
-        // to acquire the lock.
-        pthread_cond_signal(&condFuel);
-        pthread_mutex_unlock(&mutexFuel);
-        sleep(1);
+            // The order of these two calls seems to not matter for overall
+            // correctness of the program. As long as we unlock the mutex,
+            // the thread receiving the signal can react to the signal.
+            // If we don't unlock, the other thread will just block waiting
+            // to acquire the lock.
+            pthread_cond_signal(&condFuel);
+            pthread_mutex_unlock(&mutexFuel);
+            sleep(1);
+        }
     }
     return NULL;
 }
@@ -93,19 +97,26 @@ car(void * arg)
 int
 main(int argc, char * argv[])
 {
-    pthread_t th[NUM_THREADS];
+    pthread_t th[NUM_THREADS]   = {0};
+    int       tid[NUM_THREADS]  = {0};
     pthread_mutex_init(&mutexFuel, NULL);
     pthread_cond_init(&condFuel, NULL);
 
-    for (int i = 0; i < NUM_THREADS; i++) {
-        if (i == 1) {
-            if (pthread_create(&th[i], NULL, &fuel_filling, (void *) &i) != 0) {
-                perror("Failed to create fuel-filling thread");
-            }
-        } else {
-            if (pthread_create(&th[i], NULL, &car, (void *) &i) != 0) {
-                perror("Failed to create thread for car pulling fuel");
-            }
+    int tctr = 0;
+    // Start the set of car-threads, so some of them will wait before the
+    // threads that start filling fuel tanks start-up
+    for (int i = 0; i < NUM_CARS; i++, tctr++) {
+        tid[tctr] = tctr;
+        if (pthread_create(&th[i], NULL, &car, (void *) &tid[tctr]) != 0) {
+            perror("Failed to create thread for car pulling fuel");
+        }
+    }
+
+    // Start n-threads filling up the fuel tank
+    for (int i = 0; i < NUM_FILLERS; i++) {
+        tid[tctr] = tctr;
+        if (pthread_create(&th[i], NULL, &fuel_filling, (void *) &tid[tctr]) != 0) {
+            perror("Failed to create fuel-filling thread");
         }
     }
 
