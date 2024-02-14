c0fb421d07668803b362c56396f9c049f4045702
Aditya Gurajada <gapisback@gmail.com>
2024-02-14 15:04:48 -0800

    [CTCI:ll.is-palindrome] Implement isPalindrome2() using slow/fast ptrs.

--- Self-Study/CTCI/ch2.ll.is-palindrome.cpp
+++ Self-Study/CTCI/ch2.ll.is-palindrome.cpp
@@ -92,6 +92,9 @@ class LinkedList
     // Check if data in linked list is a palindrome.
     bool isPalindrome();
 
+    // Check if data in linked list is a palindrome, without relying on 'nitems'.
+    bool isPalindrome2();
+
     // Print the linked list.
     void printList();
 
@@ -179,6 +182,74 @@ LinkedList::isPalindrome() {
 }
 
 // ----------------------------------------------------
+/*
+ * Algorithm: Detect palindrome using a singly linked list.
+ *
+ * We do not maintain count of # of items in the list. Instead, determine the
+ * mid-way marker by using slow / fast pointer technique.
+ * Ref. Based on Soln #2, Pg. 164, CTCI.
+ *
+ *  - Walk forward to half-way mark of the list. 'slowp' moves forward one node
+ *    at a time, while 'fastp' moves forward 2 nodes at a time.
+ *  - Push each item to a stack till you reach 'middle' item
+ *  - Special-case handling to recognize if 'slowp' is mid-way of list with
+ *    odd or even number of items.
+ *  - Continue walking from (mid + 1)'th item to end-of-list
+ *  - Check if each new item seen == item at top-of-the-stack
+ *  - Return False, if they don't match; Else continue till eolist.
+ *  - Return True, if all matches in 2nd half match as above.
+ */
+bool
+LinkedList::isPalindrome2() {
+    if (this->isEmpty()) {
+        return true;
+    }
+    if (this->capacity() == 1) {
+        return true;
+    }
+    const int nitems = this->capacity();
+    int is_odd = (nitems % 2);
+    int found_stack[(nitems / 2)];
+    int tos = 0;    // top-of-stack index
+    bool rv = true;
+
+    // slowp moves one node ahead at a time, while fastp will move 2 nodes at a time.
+    // Intially both pointers start from the 1st node of the linked list.
+    Node *slowp = this->head;
+    Node *fastp = this->head;
+
+    // While there is more nodes to traverse 2-nodes-at-a-time, move ahead ...
+    while (slowp && fastp && fastp->next) {
+        found_stack[tos] = slowp->data;
+        tos++;
+        slowp = slowp->next;
+
+        // Check if we can skip a node from the current node that fastp is pointing to.
+        fastp = fastp->next;
+        fastp = (fastp ? fastp->next : (Node *) NULL);
+    }
+    // State of fastp tells us if there are odd # of items in the list..
+    // slowp is positioned at the the middle item if list capacity is odd.
+    if (fastp) {
+        assert(fastp->next == NULL);
+        slowp = slowp->next;
+    }
+
+    // Walk forward, checking for palindrome match with top-of-stack item
+    tos--;
+    while (tos >= 0) {
+        if (slowp->data != found_stack[tos]) {
+            rv = false;
+            break;
+        }
+        tos--;
+        slowp = slowp->next;
+    }
+
+    return rv;
+}
+
+// ----------------------------------------------------
 void
 LinkedList::printList() {
     if (this->head == NULL) {
@@ -212,6 +283,16 @@ void test_isPalindrome_two_diff_items(void);
 void test_isPalindrome_three_equal_items(void);
 void test_isPalindrome_three_diff_items(void);
 
+// Re-test with new implementation of palindrome checks.
+void test_isPalindrome2_two_equal_items(void);
+void test_isPalindrome2_two_diff_items(void);
+void test_isPalindrome2_three_equal_items(void);
+void test_isPalindrome2_three_diff_items(void);
+void test_isPalindrome2_four_equal_items(void);
+void test_isPalindrome2_four_diff_items(void);
+void test_isPalindrome2_five_equal_items(void);
+void test_isPalindrome2_five_diff_items(void);
+
 /*
  * main() and test cases begin here ...
  */
@@ -230,6 +311,16 @@ main(int argc, char *argv[])
     test_isPalindrome_two_diff_items();
     test_isPalindrome_three_equal_items();
     test_isPalindrome_three_diff_items();
+
+    // Re-test with new implementation of palindrome checks.
+    test_isPalindrome2_two_equal_items();
+    test_isPalindrome2_two_diff_items();
+    test_isPalindrome2_three_equal_items();
+    test_isPalindrome2_three_diff_items();
+    test_isPalindrome2_four_equal_items();
+    test_isPalindrome2_four_diff_items();
+    test_isPalindrome2_five_equal_items();
+    test_isPalindrome2_five_diff_items();
 }
 
 void
@@ -324,6 +415,17 @@ test_isPalindrome_two_equal_items(void)
 }
 
 void
+test_isPalindrome2_two_equal_items(void)
+{
+    cout << __func__;
+    LinkedList list;
+    list.appendToTail(2);
+    list.appendToTail(2);
+    assert(list.isPalindrome2());
+    cout << " ... OK" << endl;
+}
+
+void
 test_isPalindrome_two_diff_items(void)
 {
     cout << __func__;
@@ -331,6 +433,17 @@ test_isPalindrome_two_diff_items(void)
     list.appendToTail(2);
     list.appendToTail(3);
     assert(list.isPalindrome() == false);
+    cout << " ... OK" << endl;
+}
+
+void
+test_isPalindrome2_two_diff_items(void)
+{
+    cout << __func__;
+    LinkedList list;
+    list.appendToTail(2);
+    list.appendToTail(3);
+    assert(list.isPalindrome2() == false);
     cout << " ... OK" << endl;
 }
 
@@ -347,6 +460,18 @@ test_isPalindrome_three_equal_items(void)
 }
 
 void
+test_isPalindrome2_three_equal_items(void)
+{
+    cout << __func__;
+    LinkedList list;
+    list.appendToTail(2);
+    list.appendToTail(42);
+    list.appendToTail(2);
+    assert(list.isPalindrome2());
+    cout << " ... OK" << endl;
+}
+
+void
 test_isPalindrome_three_diff_items(void)
 {
     cout << __func__;
@@ -355,5 +480,71 @@ test_isPalindrome_three_diff_items(void)
     list.appendToTail(42);
     list.appendToTail(3);
     assert(list.isPalindrome() == false);
+    cout << " ... OK" << endl;
+}
+
+void
+test_isPalindrome2_three_diff_items(void)
+{
+    cout << __func__;
+    LinkedList list;
+    list.appendToTail(2);
+    list.appendToTail(42);
+    list.appendToTail(3);
+    assert(list.isPalindrome2() == false);
+    cout << " ... OK" << endl;
+}
+
+void
+test_isPalindrome2_four_equal_items(void)
+{
+    cout << __func__;
+    LinkedList list;
+    list.appendToTail(2);
+    list.appendToTail(42);
+    list.appendToTail(42);
+    list.appendToTail(2);
+    assert(list.isPalindrome2() == true);
+    cout << " ... OK" << endl;
+}
+
+void
+test_isPalindrome2_four_diff_items(void)
+{
+    cout << __func__;
+    LinkedList list;
+    list.appendToTail(2);
+    list.appendToTail(42);
+    list.appendToTail(42);
+    list.appendToTail(3);
+    assert(list.isPalindrome2() == false);
+    cout << " ... OK" << endl;
+}
+
+void
+test_isPalindrome2_five_equal_items(void)
+{
+    cout << __func__;
+    LinkedList list;
+    list.appendToTail(2);
+    list.appendToTail(42);
+    list.appendToTail(99);
+    list.appendToTail(42);
+    list.appendToTail(2);
+    assert(list.isPalindrome2() == true);
+    cout << " ... OK" << endl;
+}
+
+void
+test_isPalindrome2_five_diff_items(void)
+{
+    cout << __func__;
+    LinkedList list;
+    list.appendToTail(2);
+    list.appendToTail(42);
+    list.appendToTail(99);
+    list.appendToTail(42);
+    list.appendToTail(3);
+    assert(list.isPalindrome2() == false);
     cout << " ... OK" << endl;
 }
