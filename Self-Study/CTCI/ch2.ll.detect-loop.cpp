4f8b1ccb38669aeec08af74285562c65407f9a94
Aditya Gurajada <gapisback@gmail.com>
2024-02-14 11:21:58 -0800

    [CTCI:ll.detect-loop] Implement findLoop() using SET. Add tests.

--- Self-Study/CTCI/ch2.ll.detect-loop.cpp
+++ Self-Study/CTCI/ch2.ll.detect-loop.cpp
@@ -10,6 +10,7 @@
  */
 
 #include <iostream>
+#include <set>
 #include <random>
 
 using namespace std;
@@ -57,7 +58,7 @@ class Node
     // Print contents of just this node
     void
     prNode() {
-        cout << "Node=" << this
+        cout << " Node=" << this
              << ", next=" << this->next
              << ", data=" << this->data
              << endl;
@@ -84,6 +85,10 @@ class LinkedList
     // Append new randomly generated item to the end of the list.
     Node * appendRandomToTail();
 
+    // Find out if there is a loop in this linked-list. Return the ptr to the
+    // node to which some other node is pointing to, causing the loop
+    Node * findLoop();
+
     // Print the linked list.
     void printList();
 };
@@ -109,6 +114,38 @@ LinkedList::appendRandomToTail() {
 }
 
 // ----------------------------------------------------
+/*
+ * Algorithm to detect a loop in a singly linked-list:
+ *
+ * Assume that we cannot change the Node contents; i.e., while traversing the list
+ * we cannot dirty the node, by setting a bit, which will dirty cache-lines etc.
+ * We need an ancilliary struct to track 'visited nodes'. As we have to return the
+ * ptr to the node where the loop ends, create a SET to track addresses as we
+ * traverse them. If the addr of the next found is found in the SET, that's a loop.
+ */
+Node *
+LinkedList::findLoop() {
+
+    Node **nextp = &this->head;
+    Node *rnode = NULL; // Return node ptr, which is where the loop ends.
+
+    set<void *> visited = {};
+
+    // Traverse through all the nodes
+    while (*nextp) {
+        Node *nodep = *nextp;
+        if (visited.find(nodep) == visited.end()) {
+            visited.insert(nodep);
+        } else {
+            rnode = nodep;
+            break;
+        }
+        nextp = &(nodep->next);
+    }
+    return rnode;
+}
+
+// ----------------------------------------------------
 void
 LinkedList::printList() {
     if (this->head == NULL) {
@@ -118,7 +155,7 @@ LinkedList::printList() {
     Node **nextp = &this->head;
     while (*nextp) {
         Node *nodep = *nextp;
-        cout << "Node: " << nodep
+        cout << " Node: " << nodep
              << " { next=" << nodep->next
              << ", data=" << nodep->data
              << " }" << endl;
@@ -131,7 +168,12 @@ LinkedList::printList() {
 
 void test_print_empty_list(void);
 void test_appendToTail(void);
-void test_appendRandomToTail(void);
+void test_findLoop_empty_list(void);
+void test_findLoop_1node(void);
+void test_findLoop_1node_corrupted(void);
+void test_findLoop_2nodes(void);
+void test_findLoop_2nodes_corrupted_n2(void);
+void test_findLoop_2nodes_corrupted_n2_points_to_n1(void);
 
 /*
  * main() and test cases begin here ...
@@ -143,7 +185,12 @@ main(int argc, char *argv[])
 
     test_print_empty_list();
     test_appendToTail();
-    test_appendRandomToTail();
+    test_findLoop_empty_list();
+    test_findLoop_1node();
+    test_findLoop_1node_corrupted();
+    test_findLoop_2nodes();
+    test_findLoop_2nodes_corrupted_n2();
+    test_findLoop_2nodes_corrupted_n2_points_to_n1();
 }
 
 void
@@ -160,17 +207,92 @@ test_appendToTail(void)
 {
     cout << __func__;
     LinkedList list;
-    list.appendToTail(2);
+    Node *n2 = list.appendToTail(2);
     list.printList();
     cout << " ... OK" << endl;
 }
 
 void
-test_appendRandomToTail(void)
+test_findLoop_empty_list(void)
 {
     cout << __func__;
     LinkedList list;
-    list.appendRandomToTail();
-    list.printList();
+    Node *loopNode = list.findLoop();
+    assert(loopNode == NULL);
+    cout << " ... OK" << endl;
+}
+
+void
+test_findLoop_1node(void)
+{
+    cout << __func__;
+    LinkedList list;
+    Node *n1 = list.appendToTail(1);
+    Node *loopNode = list.findLoop();
+    assert(loopNode == NULL);
+    cout << " ... OK" << endl;
+}
+
+void
+test_findLoop_1node_corrupted(void)
+{
+    cout << __func__;
+    LinkedList list;
+    Node *n1 = list.appendToTail(1);
+
+    // cause loop corruption by poking next ptr
+    assert(n1->next == NULL);
+    n1->next = n1;
+
+    Node *loopNode = list.findLoop();
+    assert(loopNode == n1);
+    cout << " ... OK" << endl;
+}
+
+void
+test_findLoop_2nodes(void)
+{
+    cout << __func__;
+    LinkedList list;
+    Node *n1 = list.appendToTail(1);
+    Node *n2 = list.appendToTail(2);
+
+    Node *loopNode = list.findLoop();
+    assert(loopNode == NULL);
+    cout << " ... OK" << endl;
+}
+
+void
+test_findLoop_2nodes_corrupted_n2(void)
+{
+    cout << __func__;
+    LinkedList list;
+    Node *n1 = list.appendToTail(1);
+    Node *n2 = list.appendToTail(2);
+
+    // cause loop corruption: n2 points back to itself.
+    assert(n2->next == NULL);
+    n2->next = n2;
+
+    Node *loopNode = list.findLoop();
+    assert(loopNode == n2);
+    cout << " ... OK" << endl;
+}
+
+void
+test_findLoop_2nodes_corrupted_n2_points_to_n1(void)
+{
+    cout << __func__;
+    LinkedList list;
+    Node *n1 = list.appendToTail(1);
+    Node *n2 = list.appendToTail(2);
+
+    // cause loop corruption: n2 points back to n1.
+    assert(n1->next == n2);
+    assert(n2->next == NULL);
+    n2->next = n1;
+
+    Node *loopNode = list.findLoop();
+    assert(loopNode == n1);
     cout << " ... OK" << endl;
 }
