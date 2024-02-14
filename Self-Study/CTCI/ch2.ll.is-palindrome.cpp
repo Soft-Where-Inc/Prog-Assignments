72252337e1d2153882c00c252ef6ac5c7fab33ce
Aditya Gurajada <gapisback@gmail.com>
2024-02-14 07:05:07 -0800

    [CTCI:ll.is-palindrome] 1st working version using stack.

--- Self-Study/CTCI/ch2.ll.is-palindrome.cpp
+++ Self-Study/CTCI/ch2.ll.is-palindrome.cpp
@@ -84,8 +84,19 @@ class LinkedList
     // Append new randomly generated item to the end of the list.
     Node * appendRandomToTail();
 
+    // Is linked-list empty?
+    bool isEmpty() { return (head == NULL); }
+
+    int capacity() { return nitems; }
+
+    // Check if data in linked list is a palindrome.
+    bool isPalindrome();
+
     // Print the linked list.
     void printList();
+
+  private:
+    int nitems = 0;
 };
 
 // ----------------------------------------------------
@@ -98,6 +109,7 @@ LinkedList::appendToTail(const int d) {
         nextp = &((*nextp)->next);
     }
     *nextp = newNode;
+    nitems++;
     return newNode;
 }
 
@@ -109,10 +121,68 @@ LinkedList::appendRandomToTail() {
 }
 
 // ----------------------------------------------------
+/*
+ * Algorithm: Detect palindrome using a singly linked list.
+ *
+ *  - Maintain count of # of items in this list when inserting
+ *  - Walk forward to half-way mark of the list.
+ *      - Walk up to (nitems/2) items in list.
+ *      - If nitems is even, we are done for now.
+ *      - If nitems is odd, walk up to next 'middle' item.
+ *  - Push each item to a stack till you reach 'middle' item
+ *  - Continue walking from (mid + 1)'th item to end-of-list
+ *  - Check if each new item seen == item at top-of-the-stack
+ *  - Return False, if they don't match; Else continue till eolist.
+ *  - Return True, if all matches in 2nd half match as above.
+ */
+bool
+LinkedList::isPalindrome() {
+    if (this->isEmpty()) {
+        return true;
+    }
+    if (this->capacity() == 1) {
+        return true;
+    }
+    const int nitems = this->capacity();
+    int is_odd = (nitems % 2);
+    int found_stack[(nitems / 2)];
+    int tos = 0;    // top-of-stack index
+    bool rv = true;
+
+    Node **nextp = &this->head;
+    Node *nodep = NULL;
+    while (*nextp && (tos < (nitems / 2))) {
+        nodep = *nextp;
+        found_stack[tos] = nodep->data;
+        tos++;
+        nextp = &(nodep->next);
+    }
+    // Skip past the middle item if capacity is odd.
+    if (is_odd) {
+        nodep = *nextp;
+        nextp = &(nodep->next);
+    }
+
+    // Walk forward, checking for palindrome match with top-of-stack item
+    tos--;
+    while (tos >= 0) {
+        nodep = *nextp;
+        if (nodep->data != found_stack[tos]) {
+            rv = false;
+            break;
+        }
+        nextp = &(nodep->next);
+        tos--;
+    }
+
+    return rv;
+}
+
+// ----------------------------------------------------
 void
 LinkedList::printList() {
     if (this->head == NULL) {
-        cout << "Empty list." << endl;
+        // cout << "Empty list." << endl;
         return;
     }
     Node **nextp = &this->head;
@@ -129,9 +199,18 @@ LinkedList::printList() {
 
 // Function prototypes
 
+void test_isEmpty_list(void);
 void test_print_empty_list(void);
 void test_appendToTail(void);
+void test_capacity(void);
+void test_capacity_random5(void);
 void test_appendRandomToTail(void);
+void test_isPalindrome_empty_list(void);
+void test_isPalindrome_one_item(void);
+void test_isPalindrome_two_equal_items(void);
+void test_isPalindrome_two_diff_items(void);
+void test_isPalindrome_three_equal_items(void);
+void test_isPalindrome_three_diff_items(void);
 
 /*
  * main() and test cases begin here ...
@@ -141,33 +220,140 @@ main(int argc, char *argv[])
 {
     cout << argv[0] << ": Hello World." << endl;
 
+    test_isEmpty_list();
     test_print_empty_list();
-    test_appendToTail();
-    test_appendRandomToTail();
+    test_capacity();
+    test_capacity_random5();
+    test_isPalindrome_empty_list();
+    test_isPalindrome_one_item();
+    test_isPalindrome_two_equal_items();
+    test_isPalindrome_two_diff_items();
+    test_isPalindrome_three_equal_items();
+    test_isPalindrome_three_diff_items();
+}
+
+void
+test_isEmpty_list(void)
+{
+    cout << __func__;
+    LinkedList list;
+    assert(list.isEmpty());
+    cout << " ... OK" << endl;
 }
 
 void
 test_print_empty_list(void)
 {
-    cout << __func__ << endl;
+    cout << __func__;
     LinkedList list;
     list.printList();
+    cout << " ... OK" << endl;
 }
 
 void
 test_appendToTail(void)
 {
-    cout << __func__ << endl;
+    cout << __func__;
     LinkedList list;
     list.appendToTail(2);
     list.printList();
+    cout << " ... OK" << endl;
 }
 
 void
 test_appendRandomToTail(void)
 {
-    cout << __func__ << endl;
+    cout << __func__;
     LinkedList list;
     list.appendRandomToTail();
     list.printList();
+    cout << " ... OK" << endl;
+}
+
+void
+test_capacity(void)
+{
+    cout << __func__;
+    LinkedList list;
+    list.appendToTail(42);
+    assert(list.capacity() == 1);
+    cout << " ... OK" << endl;
+}
+
+void
+test_capacity_random5(void)
+{
+    cout << __func__;
+    LinkedList list;
+    const int nitems = 5;
+    for (auto ictr = 0; ictr < nitems; ictr++) {
+        list.appendRandomToTail();
+    }
+    assert(list.capacity() == nitems);
+    cout << " ... OK" << endl;
+}
+
+void
+test_isPalindrome_empty_list(void)
+{
+    cout << __func__;
+    LinkedList list;
+    assert(list.isPalindrome());
+    cout << " ... OK" << endl;
+}
+
+void
+test_isPalindrome_one_item(void)
+{
+    cout << __func__;
+    LinkedList list;
+    list.appendToTail(2);
+    assert(list.isPalindrome());
+    cout << " ... OK" << endl;
+}
+
+void
+test_isPalindrome_two_equal_items(void)
+{
+    cout << __func__;
+    LinkedList list;
+    list.appendToTail(2);
+    list.appendToTail(2);
+    assert(list.isPalindrome());
+    cout << " ... OK" << endl;
+}
+
+void
+test_isPalindrome_two_diff_items(void)
+{
+    cout << __func__;
+    LinkedList list;
+    list.appendToTail(2);
+    list.appendToTail(3);
+    assert(list.isPalindrome() == false);
+    cout << " ... OK" << endl;
+}
+
+void
+test_isPalindrome_three_equal_items(void)
+{
+    cout << __func__;
+    LinkedList list;
+    list.appendToTail(2);
+    list.appendToTail(42);
+    list.appendToTail(2);
+    assert(list.isPalindrome());
+    cout << " ... OK" << endl;
+}
+
+void
+test_isPalindrome_three_diff_items(void)
+{
+    cout << __func__;
+    LinkedList list;
+    list.appendToTail(2);
+    list.appendToTail(42);
+    list.appendToTail(3);
+    assert(list.isPalindrome() == false);
+    cout << " ... OK" << endl;
 }
