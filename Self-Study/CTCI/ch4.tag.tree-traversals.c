54a7afd6df75347268ac6206ad0b4e12b529e75b
Aditya Gurajada <gapisback@gmail.com>
2024-02-17 07:44:06 -0800

    [CTCI:tag.tree-traversals] Add basic mkTree(), prTree() routines.

--- Self-Study/CTCI/ch4.tag.tree-traversals.c
+++ Self-Study/CTCI/ch4.tag.tree-traversals.c
@@ -20,18 +20,29 @@ const char *Usage = "%s [ --help | test_<fn-name> ]\n";
  * Definition of basic tree Node struct.
  */
 typedef struct node {
-    struct node *   next;
+    struct node *   left;
+    struct node *   right;
     int             data;
 } Node;
+
+#define ARRAYSIZE(arr) ((int) (sizeof(arr) / sizeof(*arr)))
 
 // Function Prototypes
 Node *mkNode(const int val);
 void freeNode(Node **np);
 void prNode(const Node *np);
 
+// values is an array of 'nitems' ints.
+Node *makeTree(int *values, int nitems);
+
+void mkTree(Node *qOfNodes[], int qsize, int *values, int nitems);
+
+void prTree(Node *rootp);
+
 void test_this(void);
 void test_msg(const char *msg);
 void test_prNode(void);
+void test_mkTree_1node(void);
 
 int
 main(int argc, char *argv[])
@@ -44,6 +55,7 @@ main(int argc, char *argv[])
         test_this();
         // test_msg(hello_msg);
         test_prNode();
+        test_mkTree_1node();
     } else if (strncmp("--help", argv[1], strlen("--help")) == 0) {
         printf(Usage, argv[0]);
         return 0;
@@ -63,7 +75,8 @@ mkNode(const int val)
     if (!np) {
         return (Node *) NULL;
     }
-    np->next = NULL;
+    np->left = NULL;
+    np->right = NULL;
     np->data = val;
     return np;
 }
@@ -81,9 +94,98 @@ freeNode(Node **np)
 void
 prNode(const Node *np)
 {
-    printf("np=%p {next=%p, data=%d}\n", np, np->next, np->data);
+    printf("np=%p {left=%p, data=%d, right=%p}\n",
+            np, np->left, np->data, np->right);
 }
 
+
+/*
+ * -----------------------------------------------------------------------------
+ * makeTree(): Tree 'constructor' routine.
+ *
+ * Given an array of integers, build a binary tree, using Breadth-First 'Search'
+ * for building tree at each level.
+ * -----------------------------------------------------------------------------
+ */
+Node *
+makeTree(int *values, const int nitems)
+{
+    if (!values || !nitems) {
+        return (Node *) NULL;
+    }
+    Node *rootp = mkNode(values[0]);
+    if (!rootp) {
+        return NULL;
+    }
+    Node *qNodes[nitems];
+    memset(qNodes, 0, sizeof(qNodes));
+    qNodes[0] = rootp;
+
+    // This is where the recursive tree construction begins
+    mkTree(qNodes, 1, (values + 1), (nitems - 1));
+
+    return rootp;
+}
+
+/*
+ * -----------------------------------------------------------------------------
+ * mkTree() - BFS implementation to build tree at each level.
+ *
+ * Parameters:
+ *  qNodes  - Ptr to head of the queue
+ *  qsize   - Queue has these many items, incl 1st item in queue
+ * -----------------------------------------------------------------------------
+ */
+void
+mkTree(Node *qNodes[], int qsize, int *values, int nitems)
+{
+    // If input is exhausted, we are done.
+    if (nitems == 0) {
+        return;
+    }
+
+    Node *curr = qNodes[0];
+    assert(curr->left == NULL);
+    assert(curr->right == NULL);
+
+    curr->left = mkNode(*values);
+    values++;
+    nitems--;
+    if (nitems == 0) {
+        return;
+    }
+    curr->right = mkNode(*values);
+    values++;
+    nitems--;
+
+    // Shove two newly allocated nodes to the tail of the queue
+    qNodes[qsize++] = curr->left;
+    qNodes[qsize++] = curr->right;
+
+    // Recurse to build tree for next node at head-of-the-queue.
+    // (Therefore, queue size goes down by 1.)
+    mkTree((qNodes + 1), (qsize - 1), values, nitems);
+}
+
+/*
+ * -----------------------------------------------------------------------------
+ * prTree() - Tree printing routine.
+ * -----------------------------------------------------------------------------
+ */
+void
+prTree(Node *rootp)
+{
+    if (!rootp)
+        return;
+
+    printf("[%d]\n", rootp->data);
+    if (rootp->left) {
+        prTree(rootp->left);
+    }
+    if (rootp->right) {
+        prTree(rootp->right);
+    }
+}
 
 // **** Test cases ****
 
@@ -116,5 +218,19 @@ test_prNode(void)
     prNode(np);
     freeNode(&np);
     assert(np == NULL);
+    printf(" ... OK\n");
+}
+
+// Basic verification of tree construction and print of 1-node tree
+void
+test_mkTree_1node(void)
+{
+    printf("%s", __func__);
+
+    int values[] = {42};
+    assert(1 == 1);
+    Node *rootp = makeTree(values, ARRAYSIZE(values));
+    assert(rootp);
+    prTree(rootp);
     printf(" ... OK\n");
 }
