52f0f519191af6868bc58e31e6cb0a4cd2294a65
Aditya Gurajada <gapisback@gmail.com>
2024-02-22 16:10:19 -0800

    Initial versions of Cpp-Tutorials programs.

---
+++ Self-Study/Cpp-Tutorials/README.md
@@ -0,0 +1,7 @@
+# Self-Study/Cpp-Tutorials/README.md
+
+C++ Learning Tutorial Exercises from different sources.
+
+- A Tour of C++, Bjarne Stroustrup, 2nd edition
+
+- [YouTube](https://www.youtube.com/watch?v=xGDLkt-jBJ4&t=2346s) CppCon 2019: Arthur O'Dwyer “Back to Basics: Smart Pointers”
---
+++ Self-Study/Cpp-Tutorials/smart-pointers-CppCon-2019.cpp
@@ -0,0 +1,63 @@
+/*
+ * -----------------------------------------------------------------------------
+ * Implement example usages to understand smart pointers better, based on the
+ * material covered in this video, from CppCon 2019.
+ *
+ * Ref: https://www.youtube.com/watch?v=xGDLkt-jBJ4&t=2346s)
+ *      Arthur O'Dwyer “Back to Basics: Smart Pointers”
+ *
+ * Usage: g++ -o template-program-cpp template-program.cpp
+ *
+ * History:
+ * -----------------------------------------------------------------------------
+ */
+
+#include <iostream>
+
+using namespace std;
+
+string Usage = " [ --help | test_<fn-name> ]\n";
+
+// Function Prototypes
+void test_this(void);
+void test_msg(string&);
+
+int
+main(const int argc, const char *argv[])
+{
+    string hello_msg = "Hello World.";
+    cout << argv[0] << ": " << hello_msg << " (argc=" << argc << ")" << endl;
+
+    // Run all test cases if no args are provided.
+    if (argc == 1) {
+        test_this();
+        test_msg(hello_msg);
+    } else if (strncmp("--help", argv[1], strlen("--help")) == 0) {
+        cout << argv[0] << Usage << endl;
+        return 0;
+    } else if (strncmp("test_", argv[1], strlen("test_")) == 0) {
+    } else {
+        cout << "Unknown argument: " << argv[1] << endl;
+    }
+}
+
+// **** Test cases ****
+
+void
+test_this(void)
+{
+    cout << __func__;
+
+    assert(1 == 1);
+    cout << " ... OK" << endl;
+}
+
+void
+test_msg(string& msg)
+{
+    cout << __func__;
+
+    assert(msg == "Hello World.");
+    cout << " ... OK" << endl;
+}
+
