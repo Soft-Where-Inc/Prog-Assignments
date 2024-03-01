a99c3a220fcd3f084c13af2d5ec2c0874896f17b
Aditya Gurajada <gapisback@gmail.com>
2024-02-29 19:41:35 -0800

    [Cpp-Coro-3] Initial version of basic coroutine, coro_hello_world()
    This commit adds a working coroutine scaffolding needed to
    write a basic coro_hello_world(), exercised by test function,
    test_coro_hello_world_basic(). The coroutine is written so that
    it is always suspended, so it's 'Hello world' msg is never seen.
    
    Run: $ ./deciphering-coroutines test_coro_hello_world_basic
    
    Note: You still have memory leaks due to the partially incomplete
          coroutine implementation. On my Mac, do:
    
    $ leaks -atExit -- ./deciphering-coroutines test_coro_hello_world_basic

--- Self-Study/Cpp-Tutorials/deciphering-coroutines.cpp
+++ Self-Study/Cpp-Tutorials/deciphering-coroutines.cpp
@@ -5,6 +5,21 @@
  * Implement basic coroutines from simple examples.
  * Problem Statement: Implement Fibonacci series using coroutines.
  *
+ * Salient points about coroutines:
+ *
+ * - Think of coroutines -not- as a function that executes but as a factory
+ *   function that returns a coroutine "object", which the caller can use
+ *   to resume the coroutine. Coroutine object holds the state needed to
+ *   resume the coroutine where it was suspended / left-off during execn.
+ *
+ * - C++20, coroutines are "stack-less". I.e., when suspended, control flow
+ *   always returns to immediate caller.
+ *
+ * - You cannot tell from the outside, by just looking at the interface of a
+ *   function if it's a coroutine. They look-and-behave just like a normal fn.
+ *   It's the presence of `co_await`, `co_yield` or `co_return` that tells the
+ *   compiler to treat this function as a coroutine.
+ *
  * -----------------------------------------------------------------------------
  * Ref:
  *  [1] https://www.youtube.com/watch?v=J7fYddslH0Q, Andreas Weis
@@ -21,6 +36,7 @@
  */
 #include <iostream>
 #include <thread>
+#include <experimental/coroutine>
 
 using namespace std;
 
@@ -35,6 +51,7 @@ void test_this(void);
 void test_that(void);
 void test_msg(string);
 void test_runFiboGenerator_basic(void);
+void test_coro_hello_world_basic(void);
 
 // -----------------------------------------------------------------------------
 // List of test functions one can invoke from the command-line
@@ -48,11 +65,63 @@ TEST_FNS Test_fns[] = {
           { "test_this"                       , test_this }
         , { "test_that"                       , test_that }
         , { "test_runFiboGenerator_basic"     , test_runFiboGenerator_basic }
+        , { "test_coro_hello_world_basic"     , test_coro_hello_world_basic }
 };
 
 // Test start / end info-msg macros
 #define TEST_START()  printf("%s ", __func__)
 #define TEST_END()    printf(" ...OK\n")
+
+// Fabricate a string to track code-location of call-site.
+#define __LOC__ \
+    "[" + std::string{__func__} + ":" + std::to_string(__LINE__) + "] "
+
+/*
+ * *****************************************************************************
+ *      **** Coroutines Definitions and Implementation begins here ****
+ * *****************************************************************************
+ */
+
+/*
+ * AnyCoroReturnType{} - Definition of object returned by the coroutine.
+ *
+ * - promise_type{} is the object that remains "inside" the coroutine and is the
+ *   interfacing object between the caller and the coroutine.
+ *   - Is the "central intersection point" between caller & coroutine code.
+ *   - This object is automatically generated -by-the-compiler. You do nothing.
+ */
+struct AnyCoroReturnType {
+
+    // Minimal implementation to get compilation to work.
+    struct promise_type {
+        AnyCoroReturnType   get_return_object() { return {}; }
+
+        // Specifies that the coroutine should be initially suspended.
+        std::experimental::suspend_always initial_suspend() { return {}; }
+
+        void return_void() { }
+
+        void unhandled_exception() { }
+
+        // Specifies what happens when the coroutine termintes. Always use
+        // 'noexcept' as it's difficult to deal with exceptions when terminating.
+        std::experimental::suspend_always final_suspend() noexcept { return {}; }
+    };
+};
+
+/*
+ * Most basic, simplest coroutine which does basically nothing.
+ */
+
+AnyCoroReturnType
+coro_hello_world(void)
+{
+    cout << __LOC__
+         << "Hello World!"
+         << endl;
+    co_return;
+}
+
 
 /*
  * *****************************************************************************
@@ -78,7 +147,6 @@ void runFiboGenerator(void)
     }
     cout << endl;
 }
-
 
 /*
  * *****************************************************************************
@@ -155,3 +223,14 @@ test_runFiboGenerator_basic(void)
     TEST_END();
 }
 
+void
+test_coro_hello_world_basic(void)
+{
+    TEST_START();
+
+    cout << endl;
+    cout << __LOC__ << "Executing coro_hello_world() [ Prints nothing with suspend_always initial_suspend() ] ..." << endl;
+    coro_hello_world();
+    cout << __LOC__ << "Returned from coro_hello_world() ..." << endl;
+    TEST_END();
+}
