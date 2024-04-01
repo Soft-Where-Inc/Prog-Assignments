5d8ac42082269d0e932182cf5a3b08d1c33f1b41
Aditya P. Gurajada <adityagurajada@yahoo.com>
2024-03-31 18:18:16 -0700

    [LOC2-dump] Add locations_dump.c to dump ELF sections similar to readelf
    This commit adds a stand-alone program developed as an exercise
    to learn ELF APIs and to use them to dump ELF-sections.
    
    [LOC2-dump] Figure out layout of loc_ids section's entries.
    
    - Add debugging to figure out how the entries in the `loc_ids`
      are laid out.
    
    - Debugging shows that even though prev sizeof(struct location),
      the offsets of each entry seems to be rounded-up to 32 bytes.
      So, to ease calculation in dump functions, add more pad-bytes
      to this struct so its sizeof() becomes 32 bytes.
    
    - Add print_this_section(). Print sections for `.rodata`, `.data`
      in addition to the `loc_ids` section.
    
    - Add hexdump() and enhanced for improved debugging-ability.
    
    - dump_loc_ids(): Unpack file/func-offsets to print names.

--- Self-Study/Tools/locations.c
+++ Self-Study/Tools/locations.c
@@ -17,6 +17,6 @@ struct location loc_id_ref __attribute__((section("loc_ids")));
 
 void print_loc(int id)
 {
-   struct location *loc = (struct location *)(((intptr_t)&loc_id_ref) + id);
+   struct location *loc = (struct location *)( ( (intptr_t)&loc_id_ref ) + id );
    printf("Location is in function '%s', %s:%d\n", loc->fn, loc->file, loc->line);
 }
